ed2b7bfe77fa16b630954804100642d2f3629bb0
Andre <andrew7carlin@gmail.com>
2026-01-24 12:12:21 -0700

    Add partner website links to logos

--- src/components/ui/PartnersSection.jsx
+++ src/components/ui/PartnersSection.jsx
@@ -12,12 +12,12 @@ import homeDepot from '../../assets/Partners/the-home-depot-1-logo-black-and-whi
 import iconDark from '../../assets/logos/icon-dark.png';
 
 const partners = [
-    { name: 'AR Mays', logo: arMays },
-    { name: 'Carlisle', logo: carlisle },
-    { name: 'QXO', logo: qxo },
-    { name: 'R and O', logo: rAndO },
-    { name: 'Salad and Go', logo: saladAndGo },
-    { name: 'Home Depot', logo: homeDepot },
+    { name: 'AR Mays', logo: arMays, url: 'https://www.armays.com/' },
+    { name: 'Carlisle', logo: carlisle, url: 'https://www.carlisleconstructionmaterials.com/' },
+    { name: 'QXO', logo: qxo, url: 'https://www.qxo.com/' },
+    { name: 'R and O', logo: rAndO, url: 'https://www.randoco.com/' },
+    { name: 'Salad and Go', logo: saladAndGo, url: 'https://www.saladandgo.com/' },
+    { name: 'Home Depot', logo: homeDepot, url: 'https://www.homedepot.com/' },
 ];
 
 const PartnersSection = () => {
@@ -37,9 +37,15 @@ const PartnersSection = () => {
                 {/* Partner Logos Grid */}
                 <div className={styles.logosGrid}>
                     {partners.map((partner, index) => (
-                        <div key={index} className={styles.logoItem}>
+                        <a
+                            key={index}
+                            href={partner.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className={styles.logoItem}
+                        >
                             <img src={partner.logo} alt={partner.name} />
-                        </div>
+                        </a>
                     ))}
                 </div>
             </div>
