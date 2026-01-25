d48283b936fe5b1e703d5202d5c92977bc1bbc7c
Andre <andrew7carlin@gmail.com>
2026-01-24 17:02:08 -0700

    Wire up all leadership photos

--- src/components/ui/LeadershipSection.jsx
+++ src/components/ui/LeadershipSection.jsx
@@ -1,23 +1,39 @@
 import React, { useRef } from 'react';
 import styles from './LeadershipSection.module.css';
 
+// Leadership photos
+import kevenOtt from '../../assets/leadership/Kevin-Ott.png';
+import joeJuelfs from '../../assets/leadership/Joe-Juelfs.png';
+import jessicaGonzalez from '../../assets/leadership/Jessica-Gonzalez.png';
+import anthonyGonzalez from '../../assets/leadership/Anthony-Gonzalez.png';
+import cameronOtt from '../../assets/leadership/Cameron-Ott.png';
+import steveSampson from '../../assets/leadership/Steve-Samson.png';
+import clayFinch from '../../assets/leadership/Clay-Finch.png';
+import jeremiahDevine from '../../assets/leadership/Jeremiah-Devine.png';
+import andrewCarlin from '../../assets/leadership/Andrew-Carlin.png';
+import johnBlake from '../../assets/leadership/John-Blake.png';
+import nathanSchoolmeester from '../../assets/leadership/Nathan-Schoolmeester.png';
+import randyFinch from '../../assets/leadership/Randy-Finch.png';
+import koreyHines from '../../assets/leadership/Korey-Hines.png';
+import ashleyUngaro from '../../assets/leadership/Ashley-Ungaro.png';
+import randyPerry from '../../assets/leadership/Randy-Perry.png';
+
 const leaders = [
-    { name: 'Keven Ott', title: 'Owner', photo: null },
-    { name: 'Joe Juelfs', title: 'Owner', photo: null },
-    { name: 'Jessica Gonzalez', title: 'Owner', photo: null },
-    { name: 'Anthony Gonzalez', title: 'Owner', photo: null },
-    { name: 'Pat Carlin', title: 'Chief Operating Officer', photo: null },
-    { name: 'Cameron Ott', title: 'Commercial Manager', photo: null },
-    { name: 'Steve Sampson', title: 'Director of Roofing – Mohave County', photo: null },
-    { name: 'Clay Finch', title: 'Director of Stucco', photo: null },
-    { name: 'Jeremiah Devine', title: 'Director of Roofing – Colorado River', photo: null },
-    { name: 'Andrew Carlin', title: 'Director of Operations – Nevada, Utah', photo: null },
-    { name: 'John Blake', title: 'Director of Operations – Phoenix, New Mexico', photo: null },
-    { name: 'Nathan Schoolmeester', title: 'Director of HVAC Operations', photo: null },
-    { name: 'Randy Finch', title: 'Director of Estimating', photo: null },
-    { name: 'Korey Hines', title: 'Director of Operations, Craftwork Fence', photo: null },
-    { name: 'Ashley Ungaro', title: 'Director of Construction', photo: null },
-    { name: 'Randy Perry', title: 'Director of Plumbing', photo: null },
+    { name: 'Keven Ott', title: 'Owner', photo: kevenOtt },
+    { name: 'Joe Juelfs', title: 'Owner', photo: joeJuelfs },
+    { name: 'Jessica Gonzalez', title: 'Owner', photo: jessicaGonzalez },
+    { name: 'Anthony Gonzalez', title: 'Owner', photo: anthonyGonzalez },
+    { name: 'Cameron Ott', title: 'Commercial Manager', photo: cameronOtt },
+    { name: 'Steve Sampson', title: 'Director of Roofing – Mohave County', photo: steveSampson },
+    { name: 'Clay Finch', title: 'Director of Stucco', photo: clayFinch },
+    { name: 'Jeremiah Devine', title: 'Director of Roofing – Colorado River', photo: jeremiahDevine },
+    { name: 'Andrew Carlin', title: 'Director of Operations – Nevada, Utah', photo: andrewCarlin },
+    { name: 'John Blake', title: 'Director of Operations – Phoenix, New Mexico', photo: johnBlake },
+    { name: 'Nathan Schoolmeester', title: 'Director of HVAC Operations', photo: nathanSchoolmeester },
+    { name: 'Randy Finch', title: 'Director of Estimating', photo: randyFinch },
+    { name: 'Korey Hines', title: 'Director of Operations, Craftwork Fence', photo: koreyHines },
+    { name: 'Ashley Ungaro', title: 'Director of Construction', photo: ashleyUngaro },
+    { name: 'Randy Perry', title: 'Director of Plumbing', photo: randyPerry },
 ];
 
 const LeadershipSection = () => {
