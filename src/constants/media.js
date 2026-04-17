d363dc633f18ae40b16ea62ee3b1a7a48c1950a0
Andrew Carlin <andrew7carlin@users.noreply.github.com>
2026-04-17 13:56:09 -0700

    feat: update hero mobile video to Lobby_Presentation_9-16 (1) on BunnyCDN

--- src/constants/media.js
+++ src/constants/media.js
@@ -1,3 +1,3 @@
 export const LOBBY_VIDEO_URL = 'https://cse-media.b-cdn.net/Lobby_Presentation_16-9%20(5).mp4';
-export const LOBBY_VIDEO_MOBILE_URL = 'https://cse-media.b-cdn.net/Lobby_Presentation_9-16.mp4';
+export const LOBBY_VIDEO_MOBILE_URL = 'https://cse-media.b-cdn.net/Lobby_Presentation_9-16%20(1).mp4';
 export const IS_YOUTUBE = false;
